9466a16fa46b5214655f2ec4b8eb3b3c34244c93
22A91A05C7 <119839410+22A91A05C7@users.noreply.github.com>
2022-12-06 12:08:02 +0530

    #include<stdio.h> int main() {     int x,y,e;     scanf("%d%d",&x,&y);     e=30*y;     if(x<e)     {         printf("NO");     }     else     {         printf("YES");     } }

---
+++ Expenditure.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,e;
+    scanf("%d%d",&x,&y);
+    e=30*y;
+    if(x<e)
+    {
+        printf("NO");
+    }
+    else
+    {
+        printf("YES");
+    }
+}
