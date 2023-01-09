6d52c994d15f45a4196bd86d697414ce0a3fe02d
22A91A05C7 <119839410+22A91A05C7@users.noreply.github.com>
2023-01-09 10:24:06 +0530

    #include<stdio.h> #include<math.h> int main() { 	int a,b,c; 	scanf("%d",&a); 	b=a%4; 	c=a/4; 	if(b!=0) 	printf("%d",c+1);     else 	printf("%d",c); }

--- Cars.c
+++ Cars.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-#include<stdio.h>
+#include<math.h>
 int main()
 {
-    int a,b,c;
-    scanf("%d",&a);
-    b=a%4;
-    c=a/4;
-    if(b!=0)
-    printf("%d",c+1);
-    else
-    printf("%d",c);
+	int a,b,c;
+	scanf("%d",&a);
+	b=a%4;
+	c=a/4;
+	if(b!=0)
+	printf("%d",c+1);
+    else 
+	printf("%d",c);
 }
