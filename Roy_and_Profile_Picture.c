ebfdac3a2c40f0a0af38b2067bb78c90705b9fcb
22A91A05C7 <119839410+22A91A05C7@users.noreply.github.com>
2023-01-23 19:24:12 +0530

    #include<stdio.h> int main() {     int l,n;     scanf("%d%d",&l,&n);     while(n--)     {         int w,h;         scanf("%d%d",&w,&h);
            if(w<l|h<l)
            {
                printf("UPLOAD ANOTHER
    ");
            }
            if(w>=l&&h>=l)
            {
                if(w==h)
                {
                    printf("ACCEPTED
    ");
                }
                else
                {
                    printf("CROP IT
    ");
                }
            }
            
        }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int l,n;
+    scanf("%d%d",&l,&n);
+    while(n--)
+    {
+        int w,h;
+        scanf("%d%d",&w,&h);
+        
+        if(w<l|h<l)
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        if(w>=l&&h>=l)
+        {
+            if(w==h)
+            {
+                printf("ACCEPTED
+");
+            }
+            else
+            {
+                printf("CROP IT
+");
+            }
+        }
+        
+    }
+}
