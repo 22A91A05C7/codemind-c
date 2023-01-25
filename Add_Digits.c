c558a319a15455d7cc8fc9bedec703d89ac701c8
22A91A05C7 <119839410+22A91A05C7@users.noreply.github.com>
2023-01-25 10:32:34 +0530

    #include<stdio.h> int main() {     int n,r,sum;     scanf("%d",&n);     while(n/10!=0)     {         sum=0;         while(n!=0)         {             r=n%10;             sum=sum+r;             n=n/10;         }         n=sum;     }     printf("%d",sum); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,42 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int n,s=0,r;
+    int n,r,sum;
     scanf("%d",&n);
-    while(n>0)
+    while(n/10!=0)
     {
-        r=n%10;
-        s=s+r;
-        n=n/10;
-    }
-    int t=s,rem,sum=0,r2,s2=0;
-    if(s>9)
-    {
-        while(s>0)
+        sum=0;
+        while(n!=0)
         {
-            rem=s%10;
-            sum=sum+rem;
-            s=s/10;
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
         }
-        int temp=sum;
-        if(sum<10)
-        {
-            printf("%d",sum);
-        }
-        else
-        {
-            while(sum>0)
-            {
-                r2=sum%10;
-                s2=s2+r2;
-                sum=sum/10;
-            }
-            printf("%d",s2);
-            
-        }
+        n=sum;
     }
-    else
-    {
-        printf("%d",s);
-    }
+    printf("%d",sum);
 }
